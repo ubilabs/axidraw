57c53b12a854df7097e4dbf074d4a1e8a2b51951
Martin Kleppe <kleppe@ubilabs.net>
2018-05-15 15:04:13 +0200

    draw multiple paths

--- pen-api.js
+++ pen-api.js
@@ -1,17 +1,51 @@
 const axidraw = {
-
   ratio: 12000 / 8720, // http://localhost:4242/v1/settings/bot
 
   init: async () => {
+    // release pen and stop motor
     await axidraw.pen("state=up");
     await axidraw.call("DELETE", 'motors');
   },
 
-  call: (method, endpoint, action, callback) => {
+  drawPath: (coords) => {
+    const batch = ['state=up'];
+    let penUp = true;
 
+    coords.forEach((coord, index) => {
+      if (coord === null) {
+        batch.push('state=up');
+        penUp = true;
+      } else {
+        const [x, y] = coord;
+        batch.push(`x=${x}&y=${y*axidraw.ratio}`);
+
+        if (penUp) {
+          batch.push('state=draw');
+          penUp = false;
+        }
+      }
+
+      if (index == 0) {
+        batch.push('state=draw')
+      }
+    });
+
+    batch.push('state=up');
+    axidraw.batch(batch);
+  },
+
+  /**
+   * Internally used. Calls the RoboPaint API.
+   *
+   * @param  {String} method the HTTP eg "PUT" or "DELETE"
+   * @param  {String} endpoint the API endpoint eg "pen" or "motors"
+   * @param  {String} data the data to sent. Must be a form URL encoded format.
+   * @param  {Function} callback Optional. The callback to execute. Will return a promise if not provided.
+   */
+  call: (method, endpoint, data, callback) => {
     if (!callback) {
       return new Promise(function (resolve, reject) {
-        axidraw.call(method, endpoint, action, resolve);
+        axidraw.call(method, endpoint, data, resolve);
       });
     }
 
@@ -29,41 +63,33 @@ const axidraw = {
       }
     }
 
-    xhr.send(action);
+    xhr.send(data);
   },
 
-  pen: (action, callback) => {
-    return axidraw.call("PUT", 'pen', action, callback);
+
+  /**
+   * Simple wrapper to send data to the pen. Internally used.
+   * @param  {String} data The pen data eg "x=1&y=2" or "state=up"
+   * @param  {Function} callback The callback
+   */
+  pen: (data, callback) => {
+    return axidraw.call("PUT", 'pen', data, callback);
   },
+
 
   batch: (batch) => {
 
     function next() {
-      const coords = batch.shift();
-      if (coords) {
-        axidraw.pen(coords, next);
+      const action = batch.shift();
+      console.log(action);
+      if (action) {
+        axidraw.pen(action, next);
       } else {
         axidraw.init();
       }
     }
 
     next();
-  },
-
-  drawPath: (coords) => {
-    const batch = ['state=up'];
-
-    coords.map((coord, index) => {
-      const [x, y] = coord;
-      batch.push(`x=${x}&y=${y*axidraw.ratio}`);
-
-      if (index == 0) {
-        batch.push('state=draw')
-      }
-    });
-
-    batch.push('state=up');
-    axidraw.batch(batch);
   }
 }
 
