68c078692157b0fe0f95b55df4eed85623aa8330
Franz Neubert <neubert@ubilabs.net>
2018-05-22 10:44:38 +0200

    feat(api-test): use new Axidraw api

--- src/api-test.js
+++ src/api-test.js
@@ -1,19 +1,14 @@
-import axidraw from './lib/pen-api';
+import createAxidraw from './lib/axidraw';
 
 (async function() {
-  await axidraw.init();
+  const axidraw = await createAxidraw();
+  const lines = [
+    [[1, 1], [2, 1], [2, 2]],
+    [[3, 1], [4, 1], [4, 2]],
+    [[3, 3], [4, 3], [4, 4]]
+  ];
 
-  axidraw.drawPath([
-    [1, 1],
-    [2, 1],
-    [2, 2],
-    null,
-    [3, 1],
-    [4, 1],
-    [4, 2],
-    null,
-    [3, 3],
-    [4, 3],
-    [4, 4]
-  ]);
+  for (const line of lines) {
+    await axidraw.drawPath(line);
+  }
 })();
