e2cc612735dbd3176c25c1b69a6d1dff7b3b5ea3
Malte Modrow <modrow@ubilabs.net>
2018-05-25 14:57:58 +0200

    feat(preview): add geocoder and connect plotting to map

--- src/d3-svg-map.js
+++ src/d3-svg-map.js
@@ -1,73 +1,8 @@
-import {geoPath, select, line} from 'd3';
+import {plotLines} from './lib/plot-lines';
 
-import createAxidraw from './lib/axidraw';
-import getProjection from './lib/get-projection';
-import {optimizeOrder} from './lib/optimize-lines';
-import loadLines from './lib/load-lines';
-import mergeLines from './lib/merge-lines';
-import simplifyLines from './lib/simplify-lines';
-import {renderSVGPaths} from './lib/svg-tools';
-import cropLines from './lib/crop-lines';
-
-const height = 100;
 const width = 200;
+const height = 100;
 const zoom = 18;
 const center = [9.995, 53.565];
 
-const viewBox = [0, 0, width, height];
-
-async function plotLines(lines) {
-  const axidraw = await createAxidraw();
-  const project = getProjection({
-    width,
-    height,
-    zoom,
-    center
-  });
-
-  const projectedLines = lines.map(line => line.map(project));
-  const croppedLines = cropLines(projectedLines, viewBox);
-  const mergedLines = mergeLines(croppedLines);
-  const simplifiedLines = simplifyLines(mergedLines);
-  const svgPaths = renderSVGPaths(simplifiedLines);
-
-  document.getElementById('map').innerHTML = svgPaths.join('\n');
-
-
-  function logStats(label, lines) {
-    console.log(`
-      ${label}:
-      ${lines.length} lines
-      ${lines.reduce((acc, line) => acc + line.length, 0)} points
-    `);
-  }
-
-  logStats('original', projectedLines);
-  logStats('cropped', croppedLines);
-  logStats('merged', mergedLines);
-  logStats('simplified', simplifiedLines);
-
-  for (const line of simplifiedLines) {
-    const relativeLine = line.map(p => [
-      p[0] / width * 100,
-      p[1] / height * 100
-    ]);
-
-    await axidraw.drawPath(relativeLine);
-  }
-}
-
-(async function() {
-  const viewport = {
-    width,
-    height,
-    zoom,
-    center
-  };
-  const projection = getProjection(viewport);
-  const geojsonToPath = geoPath(projection);
-
-  const sortedLines = optimizeOrder(await loadLines(viewport));
-
-  plotLines(sortedLines);
-})();
+plotLines({width, height, zoom, center}, document.querySelector('#map'));
---
+++ src/lib/plot-lines.js
@@ -0,0 +1,47 @@
+import createAxidraw from './axidraw';
+import getProjection from './get-projection';
+import {optimizeOrder} from './optimize-lines';
+import loadLines from './load-lines';
+import mergeLines from './merge-lines';
+import simplifyLines from './simplify-lines';
+import {renderSVGPaths} from './svg-tools';
+import cropLines from './crop-lines';
+
+export async function plotLines(viewport, debugSVG) {
+  const axidraw = await createAxidraw();
+  const project = getProjection(viewport);
+  const viewBox = [0, 0, viewport.width, viewport.height];
+
+  const sortedLines = optimizeOrder(await loadLines(viewport));
+  const projectedLines = sortedLines.map(line => line.map(project));
+  const croppedLines = cropLines(projectedLines, viewBox);
+  const mergedLines = mergeLines(croppedLines);
+  const simplifiedLines = simplifyLines(mergedLines);
+
+  if (debugSVG) {
+    const svgPaths = renderSVGPaths(simplifiedLines);
+    debugSVG.innerHTML = svgPaths.join('\n');
+  }
+
+  function logStats(label, lines) {
+    console.log(`
+      ${label}:
+      ${lines.length} lines
+      ${lines.reduce((acc, line) => acc + line.length, 0)} points
+    `);
+  }
+
+  logStats('original', projectedLines);
+  logStats('cropped', croppedLines);
+  logStats('merged', mergedLines);
+  logStats('simplified', simplifiedLines);
+
+  for (const line of simplifiedLines) {
+    const relativeLine = line.map(p => [
+      p[0] / viewport.width * 100,
+      p[1] / viewport.height * 100
+    ]);
+
+    await axidraw.drawPath(relativeLine);
+  }
+}
