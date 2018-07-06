856b24eb360b396ebe421e1f0df877a5116974ba
Robert Katzki <katzki@ubilabs.net>
2018-07-06 14:35:02 +0200

    feat: add styling to the html pages

--- src/adjust-pen.js
+++ src/adjust-pen.js
@@ -14,8 +14,8 @@ async function initPosition() {
   await axidraw.resetMotor();
 
   info.innerHTML = `
-    Axidraw ready. <br />
-    Move robot to the initial position [0, 0].
+    AxiDraw is ready! <br /> <br />
+    Move robot to the initial position [0, 0] (most likely top left).
   `;
 
   button.disabled = false;
@@ -25,15 +25,20 @@ async function initPosition() {
 async function adjustPen() {
   info.innerHTML = `Setting pen state down…`;
   await axidraw.setPenState('state=0.8');
-  info.innerHTML = `Please insert pen at lowest position.`;
+  info.innerHTML = `Please insert pen at lowest position, touching the surface.`;
   button.onclick = parkPen;
 }
 
 async function parkPen() {
   info.innerHTML = `Setting pen state up…`;
   await axidraw.setPenState('state=up');
-  info.innerHTML = `Done. You are now ready to draw!`;
-  button.disabled = true;
+  info.innerHTML = `Done. You are now ready to draw!<br /><br />You should now <a href="/draw-bounds.html" title="Draw the bounds">draw the bounds</a>.`;
+  button.onclick = goToDrawBounds;
+  button.innerHTML = 'Draw the bounds →'
+}
+
+async function goToDrawBounds() {
+  window.location = '/draw-bounds.html';
 }
 
 window.addEventListener("load", initAxidraw);
